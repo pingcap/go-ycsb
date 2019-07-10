4d4c36a18023795203538d92aaeda98de8b43a2f
xiaojingchen <chenxiaojing141@gmail.com>
2019-07-10 14:49:15 +0800

    fix comma bug

--- cmd/go-ycsb/client.go
+++ cmd/go-ycsb/client.go
@@ -73,7 +73,7 @@ var (
 
 func initClientCommand(m *cobra.Command) {
 	m.Flags().StringSliceVarP(&propertyFiles, "property_file", "P", nil, "Spefify a property file")
-	m.Flags().StringSliceVarP(&propertyValues, "prop", "p", nil, "Specify a property value with name=value")
+	m.Flags().StringArrayVarP(&propertyValues, "prop", "p", nil, "Specify a property value with name=value")
 	m.Flags().StringVar(&tableName, "table", "", "Use the table name instead of the default \""+prop.TableNameDefault+"\"")
 	m.Flags().IntVar(&threadsArg, "threads", 1, "Execute using n threads - can also be specified as the \"threadcount\" property")
 	m.Flags().IntVar(&targetArg, "target", 0, "Attempt to do n operations per second (default: unlimited) - can also be specified as the \"target\" property")
