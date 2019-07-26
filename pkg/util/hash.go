c4cf898501ca62d3efcc98bd938d419271992bf2
bb7133 <bb7133@gmail.com>
2019-07-26 20:40:26 +0800

    pkg/util: replace FNV1a with Murmur3 in hash.go

--- pkg/util/hash.go
+++ pkg/util/hash.go
@@ -15,14 +15,15 @@ package util
 
 import (
 	"encoding/binary"
-	"hash/fnv"
+
+	"github.com/spaolacci/murmur3"
 )
 
 // Hash64 returns a fnv Hash of the integer.
 func Hash64(n int64) int64 {
 	var b [8]byte
 	binary.BigEndian.PutUint64(b[0:8], uint64(n))
-	hash := fnv.New64a()
+	hash := murmur3.New64()
 	hash.Write(b[0:8])
 	result := int64(hash.Sum64())
 	if result < 0 {
@@ -33,14 +34,14 @@ func Hash64(n int64) int64 {
 
 // BytesHash64 returns the fnv hash of a bytes
 func BytesHash64(b []byte) int64 {
-	hash := fnv.New64a()
+	hash := murmur3.New64()
 	hash.Write(b)
 	return int64(hash.Sum64())
 }
 
 // StringHash64 returns the fnv hash of a string
 func StringHash64(s string) int64 {
-	hash := fnv.New64a()
+	hash := murmur3.New64()
 	hash.Write(Slice(s))
 	return int64(hash.Sum64())
 }
