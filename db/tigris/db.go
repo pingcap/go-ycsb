77649c37bdbea897b47bad12f57c46d9ea1bdf9d
pboros <pboros@tigrisdata.com>
2022-07-20 08:36:39 +0100

    use ReadOne

--- db/tigris/db.go
+++ db/tigris/db.go
@@ -65,11 +65,10 @@ func (t *tigrisDB) Read(ctx context.Context, table string, key string, fields []
 		readMap[fieldName] = true
 	}
 
-	it, err := collection.Read(ctx, filter.Eq("Key", key), read)
+	_, err := collection.ReadOne(ctx, filter.Eq("Key", key), read)
 	if err != nil {
 		return nil, fmt.Errorf("Error while reading key %s.", key)
 	}
-	it.Close()
 	return nil, nil
 }
 
