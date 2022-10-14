7583d6981dae449563aab98de909a693632ef0c1
filipecosta90 <filipecosta.90@gmail.com>
2022-10-14 18:03:41 +0100

    Delete/Update/Insert operations for dynamodb

--- cmd/go-ycsb/main.go
+++ cmd/go-ycsb/main.go
@@ -72,6 +72,8 @@ import (
 	_ "github.com/pingcap/go-ycsb/db/elasticsearch"
 	// Register etcd
 	_ "github.com/pingcap/go-ycsb/db/etcd"
+	// Register dynamodb
+	_ "github.com/pingcap/go-ycsb/db/dynamodb"
 )
 
 var (
